2afeb0d406079a1d0e53ce1741e6e6eebda3f4f3
Shubham Mogha <shubham.mogha97@gmail.com>
2023-10-31 21:12:30 +0530

    React project up using webapck

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import './index.css'
+
+const App=()=>{
+  return(
+    <div>
+    <h1 className="head">App Componnet Hello world</h1>
+    </div>
+  )
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port:3000
+  },
+};
